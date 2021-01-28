142a803cc5ee99c5137827efec5838bfc7bc4d34
Asafa Makan'a <asafajr96@gmail.com>
2021-01-28 13:52:42 +0200

    Mock Products

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Grid} from '@material-ui/core';
+
+import Product from './Product/Product';
+
+const products = [
+    {id:1, name: 'Shoes', description: "Running shoes.", 
+    image: 'https://images.unsplash.com/photo-1491553895911-0055eca6402d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80', price:"$90"},
+    {id:2, name: 'Macbook', description: "Apple Macbook.", image: 'https://images.unsplash.com/photo-1606248897020-696650b29ee1?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1400&q=80', price:"$1900"},
+];
+
+const Products = () => {
+    return(
+        <main>
+         <Grid container justify="center" spacing={4}>
+             {products.map((product) => (
+                <Grid item key ={product.id} xs={12} sm={6} md={4} lg={3}>
+                    <Product product={product} />
+                </Grid>
+             ))}
+         </Grid>
+     </main>
+    );  
+}
+
+export default Products;
