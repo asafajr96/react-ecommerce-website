4b89a917cba9dbef6b21ea42b8eb11ae31ad219e
Asafa Makan'a <asafajr96@gmail.com>
2021-02-10 14:57:07 +0200

    checkout steps

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 import { commerce } from './lib/commerce';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import {Products, Navbar, Cart} from './components';
+import {Products, Navbar, Cart, Checkout} from './components';
 
 const App = () => {
 
@@ -61,6 +61,10 @@ const App = () => {
 
                     <Route exact path="/cart">
                     <Cart cart={cart} onUpdateCartQty={handleUpdateCartQty} onRemoveFromCart={handleRemoveFromCart} onEmptyCart={handleEmptyCart} />   
+                    </Route>
+
+                    <Route exact path="/checkout">
+                        <Checkout />
                     </Route>
                 </Switch>
             </div>
